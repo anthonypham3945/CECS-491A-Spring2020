284475f5aa56f7bfac3ebee54c69a5aa2f5ceb25
Logias <ernieargel@gmail.com>
2020-04-03 13:57:33 -0700

    added main activity screen w/ a signout button

--- Code/QuikPik/app/src/main/java/com/example/quikpik/MainActivity.java
+++ Code/QuikPik/app/src/main/java/com/example/quikpik/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.quikpik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnSignOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignOut = (Button) findViewById(R.id.sign_out_button);
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUI();
+            }
+        });
+    }
+
+    /**
+     * update main activity screen and switches to login screen
+     */
+    private void updateUI() {
+        Intent newIntent = new Intent(this, Login.class);
+        startActivity(newIntent);
     }
 }
