9bb3ebb1a7d263a922ad18e75c645f8be87c61e7
Juan Villa <37051168+EcahRS@users.noreply.github.com>
2020-09-08 18:34:06 -0700

    Yelp API in Python

--- YelpAPI/sample.py
+++ YelpAPI/sample.py
@@ -1,60 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Yelp Fusion API code sample.
-
-This program demonstrates the capability of the Yelp Fusion API
-by using the Search API to query for businesses by a search term and location,
-and the Business API to query additional information about the top result
-from the search query.
-
-Please refer to http://www.yelp.com/developers/v3/documentation for the API
-documentation.
-
-This program requires the Python requests library, which you can install via:
-`pip install -r requirements.txt`.
-
-Sample usage of the program:
-`python sample.py --term="bars" --location="San Francisco, CA"`
-"""
 from __future__ import print_function
-
 import argparse
 import json
 import pprint
 import requests
 import sys
 import urllib
-
-
-# This client code can run on Python 2.x or 3.x.  Your imports can be
-# simpler if you only need one of those.
 try:
-    # For Python 3.0 and later
     from urllib.error import HTTPError
     from urllib.parse import quote
     from urllib.parse import urlencode
 except ImportError:
-    # Fall back to Python 2's urllib2 and urllib
     from urllib2 import HTTPError
     from urllib import quote
     from urllib import urlencode
 
-
-# Yelp Fusion no longer uses OAuth as of December 7, 2017.
-# You no longer need to provide Client ID to fetch Data
-# It now uses private keys to authenticate requests (API Key)
-# You can find it on
-# https://www.yelp.com/developers/v3/manage_app
 API_KEY= "87rWqTpWudO5E43v8nxXUGUXZDGyvIm589EtXD7ildImo2NQiOe-VRyfr4BMt6GOkKDfeKLqD06wo4EgGgFUjkBr68p0PUrx5p_HqvHyoS8EuDSaXAAEroehQQlgXnYx"
 
 
-# API constants, you shouldn't have to change these.
 API_HOST = 'https://api.yelp.com'
 SEARCH_PATH = '/v3/businesses/search'
-BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
+BUSINESS_PATH = '/v3/businesses/'
 
-
-# Defaults for our simple example.
 DEFAULT_TERM = 'tacos'
 DEFAULT_LOCATION = 'Long Beach, CA'
 SEARCH_LIMIT = 3
@@ -98,7 +64,6 @@ def search(api_key, term, location):
     Returns:
         dict: The JSON response from the request.
     """
-
     url_params = {
         'term': term.replace(' ', '+'),
         'location': location.replace(' ', '+'),
