39cbeb90adc4ba27feb1a3be24868794cda9111b
Juan Villa <37051168+EcahRS@users.noreply.github.com>
2020-11-03 20:48:39 -0800

    User Recommendations based on Location and Preferences

--- Code/QuikPik/app/src/main/java/com/example/quikpik/YelpActivity.java
+++ Code/QuikPik/app/src/main/java/com/example/quikpik/YelpActivity.java
@@ -1,24 +1,28 @@
 package com.example.quikpik;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
+
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -53,7 +57,43 @@ public class YelpActivity extends AppCompatActivity {
         Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
         YelpServiceInterface service = retrofit.create(YelpServiceInterface.class);
         //initiates network request
-        service.getTasks(API_KEY, "Tacos", "California").enqueue(new retrofit2.Callback<YelpSearchResult>() {
+
+        Double lat=0.0;
+        Double lng=0.0;
+        String city = "Mountain View";
+        String state = "CA";
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Location location = lm.getLastKnownLocation(lm.GPS_PROVIDER);
+            int radius = 1000;
+            lng = location.getLongitude();
+            lat = location.getLatitude();
+        }
+
+        Geocoder gcd = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = gcd.getFromLocation(lat, lng, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (addresses.size() > 0) {
+            city = (addresses.get(0).getLocality());
+            state = (addresses.get(0).getAdminArea());
+        }
+        else {
+
+        }
+
+        //COMBINES CITY AND STATE
+        String searchLocation = city + state;
+        String[] foods = {"Tacos",  "Pizza", "Chicken"};
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 2 + 1);
+
+        //API CALL TO SEARCH FOR LOCATIONS
+        service.getTasks(API_KEY, foods[randomNum], searchLocation).enqueue(new retrofit2.Callback<YelpSearchResult>() {
+
             @Override
             public void onResponse(Call<YelpSearchResult> call, Response<YelpSearchResult> response) {
                 //Log.i(TAG, response.toString());
